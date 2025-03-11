fa900070e11dd29eebf0a4c6cf74f34b2b6fa9e4
PromKunach <pr0m89215375@gmail.com>
2025-03-11 21:40:47 +0700

    Updated website files

--- script.js
+++ script.js
@@ -1,67 +1,181 @@
-const original_list = `Bribe : ติดสินบน (v.) สินบน (n.)
+
+
+const original_list = `Bribe : ติดสินบน (v.) 
 Detention : การคุมขัง (n.)
-Levy : การเก็บภาษี (n.) จัดเก็บ (v.) 
+Levy : การเก็บภาษี (n.) 
 A great deal : เยอะ มาก (adj.)
 Abide : ยอมปฏิบัติตาม (v.)
-Abbreviation : การย่อคำ
-Jeopardy : อันตราย 
+Abbreviation : การย่อคำ (n.)
+Jeopardy : อันตราย (n.)
 Abound : มีมากมาย ชุกชุม (v.)
 Paradigm : เเบบจำลอง ตัวอย่าง (n.)
-Hallmark :  เครื่องหมายรับรอง
-Damp : ความชื้น ชื้น ทำให้ชื้น
-Abolish : เลิกล้ม
-Salvage : การกู้ภ้ย กอบกู้
-Canvass : การหาเสียง
-Eager : ความกระตือรือร้น
-Shrewdness : ความเฉลียวฉลาด
-Testimonail : หนังสือรับรอง
-Shrug off : ไม่เอาใจใส่
-Periodic : บางครั้งบางคราว
-Omit : ละเว้น ละเลย
-Marginalize : ทำให้ไม่สำคัญ
-Incline : มีเเนวโน้ม
-Legisator : ผู้บัญญัติกฎหมาย
-Leisure : เวลาว่าง
-Permeate : ซึมผ่าน
-Periodical : วารสาร อย่างเป็นครั้งคราว
-Inbound : มาถึง
-Variation : การเปลี่ยนเเปลง
-Shoplifting : การขโมยของตามร้านค้า
-Reception : การรับรอง
-Pick on : การเหน็บเเนม
-Deliberate : ตั้งใจ
-Coherent : ซึ่งสอดคล้องกัน
-Cold call : โทรศัพท์มาเพื่อขายสินค้า
-Delegate : มอบหมายให้ทำเเทน
-Persistant : ดื้อดึง
-Coalition : สัมพันธมิตร
-Cognition : การรับรู้
-Harsh : รุนเเรง
-Adverse : ไม่พึงประสงค์
-Admonish : ตักเตือน 
-Acclimate : ปรับตัวให้ชินกับสภาพเเวดล้อม
-Carry out : ทำให้สำเร็จ
-Handicap : พิการ
-Garner : สะสม รวบรวม
-Impedec: ขัดขวาง`;
+Hallmark :  เครื่องหมายรับรอง (n.)
+Damp : ความชื้น (n.)
+Abolish : เลิกล้ม (v.)
+Salvage : กอบกู้ (v.)
+Canvass : การหาเสียง (n.)
+Eager : ความกระตือรือร้น (n.)
+Shrewdness : ความเฉลียวฉลาด (n.)
+Testimonail : หนังสือรับรอง (n.)
+Shrug off : ไม่เอาใจใส่ (phrav.)
+Periodic : บางครั้งบางคราว (adj.)
+Omit : ละเว้น (v.)
+Marginalize : ทำให้ไม่สำคัญ (v.)
+Incline : มีเเนวโน้ม (n.)
+Legisator : ผู้บัญญัติกฎหมาย (n.)
+Leisure : เวลาว่าง (n.)
+Permeate : ซึมผ่าน (v.)
+Periodical : วารสาร (n.)
+Inbound : มาถึง (v.)
+Variation : การเปลี่ยนเเปลง (n.)
+Shoplifting : การขโมยของตามร้านค้า (n.)
+Reception : การรับรอง (n.)
+Pick on : การเหน็บเเนม (phrav.)
+Deliberate : ตั้งใจ (v.)
+Coherent : ซึ่งสอดคล้องกัน (adj.)
+Cold call : โทรศัพท์มาเพื่อขายสินค้า (n.)
+Delegate : มอบหมายให้ทำเเทน (v.)
+Persistent : ดื้อดึง (adj.)
+Coalition : สัมพันธมิตร (n.)
+Cognition : การรับรู้ (n.)
+Harsh : รุนเเรง (adj.)
+Adverse : ไม่พึงประสงค์ (adj.)
+Admonish : ตักเตือน (v.)
+Acclimate : ปรับตัวกับสภาพเเวดล้อม (v.)
+Banquet : งานเลี้ยง (n.)
+Carry out : ทำให้สำเร็จ (phrav.)
+Handicap : อุปสรรค (n.)
+Garner : รวบรวม (v.)
+Impede : ขัดขวาง (v.)
+Elaborate : ละเอียด (adj.)
+Tariff : ภาษีศุลกากร (n.)
+Garment : เสื้อผ้า (n.)
+Abruptly : อย่างทันทีทันใด (adv.)
+Scarcity : ความขาดเเคลน (n.)
+Savvy : ความรู้ความสามารถ (n.)
+Parcel : พัสดุ (n.)
+Halt : การหยุดชะงัก (n.)
+Parliament : รัฐสภา (n.)
+Unanimous : ซึ่งหยุดชะงัก (adj.)
+Blunder : ทำพลาด (v.)
+Indulge : ดื่มด่ำ (v.)
+Aisle : ทางเดินระหว่างที่นั่ง (n.)
+Enormous : ใหญ่ (adj.)
+Allay : บรรเทา (v.)
+Neglect : การละเลย (n.)
+Sluggish : พอเพียง (n.)
+Headway : ความก้าวหน้า(n.)
+Liability : หนี้สิน (n.)
+Adequate : พอเพียง (v.)
+Alleviate : บรรเทา (adj.)
+Catastrophic : ที่เลวร้าย (adj.)
+Commence : เริ่มต้น (v.)
+Compensate : ชดเชย (v.)
+Comprehensive : ครอบคลุม (adj.)
+Compromise : ประณีประนอม (v.)
+Conseal : ปกปิด (v.)
+Consensus : เห็นพ้องกัน (v.)
+Constitude : ประกอบด้วย (v.)
+Conventional : ตามธรรมเนียม (adj.)
+Crucial : สำคัญ (adj.)
+Deficiency : การขาดแคลน (n.)
+Distinct : ชัดเจน (adj.)
+Diverse : หลากหลาย (adj.)
+Detrimental : เป็นผลเสีย (adj.)
+Apparent : เห็นได้ชัด (adj.)
+Diminish : ลดลง (v.)
+Discriminate : เลือกปฏิบัติ (v.)
+Ethical : มีจริยธรรม (adj.)
+Evaluate : ประเมินผล (v.)
+Evident : ชัดเจน (adj.)
+Exaggerate : กล่าวเกินจริง (v.)
+Expertise : ความเชี่ยวชาญ (n.)
+Exploit : แสวงหาผลกำไร (v.)
+Feasible : เป็นไปได้ (adj.)
+Hypothesis : สมมติฐาน (n.)
+Inferior : เเย่กว่า (adj.)
+Initiate : ริเริ่ม (v.)
+Integrate : รวมเข้าด้วยกัน (v.)
+Obsolate : ล้าสมัย (adj.)
+Persistent : ยืนกราน (v.)
+Intriguing : น่าติดตาม (adj.)
+Justify : ให้เหตุผล (v.)
+Negligible : เล็กน้อย (adj.)
+Profound : ลึกซึ้ง (adj.)
+Prosperty : ความเจริญรุ่งเรือง (n.)
+Reinforce : เสริมสร้าง (v.)
+Subsequent : ภายหลัง (adj.)
+Substantial : มากมาย (adj.)
+Allocate : จัดสรร (v.)
+Prevalent : พบได้บ่อยๆ (adj.)
+Soar : ทะยานขึ้น (v.)
+Foster : ส่งเสริม (v.)
+Disperse : กระจาย (v.)
+Diminish : ลดลง (v.)
+Scrutinize : ตรวจสอบอย่างละเอียด (v.)
+Punctual : ตรงต่อเวลา (adj.)
+Rekon : คิด (v.)
+Endeavor : ความพยายาม (n.)
+Convey : เเสดง (v.)
+Deteriorate : เสื่อมโทรม (v.)
+Affluent : ร่ำรวย (adj.)
+Impartial : ไม่เลือกข้าง (adj.)
+Vague : ไม่ชัดเจน (adj.)
+Tremendous : มหาศาล (adj.)
+Impoverished : ยากจน (adj.)
+Tranquil : นิ่งสงบ (adj.)
+Cunning : ฉลาดเเกมโกง (adj.)
+Inquire : สอบถาม (v.)
+Enthusiam : กระตือรือร้น (adj.)
+Ponder : คิด (v.)
+Depict : เเสดง (v.)
+Perceive : รับรู้ (v.)
+Rectify : แก้ไข (v.)
+Coerce : บังคับ (v.)
+Alienate : เเยกกัน (v.)
+Consolid : รวม (v.)
+Mitigate : บรรเทา (v.)
+Exacerbate : ทำให้เเย่ลง (v.)
+Tendency : เเนวโน้ม (n.)
+Deplete : ใช้จนหมด (v.)
+Concur : เห็นด้วย (v.)
+Emphasize : เน้นย้ำ (v.)
+Advocate : สนับสนุน (v.)
+Astonish : ทำให้ประหลาดใจ (v.)
+Concede : ยอมแพ้ (v.)
+Authentic : เเท้จริง (adj.)
+Tangible : จับต้องได้ (adj.)
+Sporadic : เกิดไม่บ่อย (adj.)
+Mandatory : ที่ต้องมี (adj.)
+Ailment : เจ็บป่วย (n.)
+Infer : สรุป (v.)
+Sincere : จริงใจ (adj.)
+Relictant : ฝืนใจ (v.)
+Interpret : อธิบาย (v.)
+Set in stone : ไม่สามารถเปลี่ยนได้ (phrav.)
+Superstition : ไสยศาสตร์ (n.)`;
 
 const splitted_line = original_list.split("\n");
 let current_number = 0; 
+
+
 function print_split(){
     let word_text = document.getElementById("word-eng");
-    let meaning_text = document.getElementById("meaning");  
-    meaning_text.textContent = "";
-    meaning_text.style.display = "none";
+    let meaning_text = document.getElementById("meaning");
+    let count_text = document.getElementById("timer");  
+    
+
         let word = splitted_line[current_number].split(":")[0];
         let meaning = splitted_line[current_number].split(":")[1];
         console.log(word, "=",meaning);
-       
         word_text.textContent = word;
-      
+        count_text.textContent = (current_number+1 + " / " + (splitted_line.length+1))
+        meaning_text.textContent = meaning;
+  
     
 }
 
-
+print_split();
 function add(){
     current_number += 1;
     print_split();
@@ -71,9 +185,23 @@ function subtract(){
     current_number -= 1;
     print_split();
 }
+
+let reveal_mode = 0;
+
 function reveal(){
     let meaning_text = document.getElementById("meaning");  
-    let meaning = splitted_line[current_number].split(":")[1];
-    meaning_text.textContent = meaning;
-    meaning_text.style.display = "block";
+  
+    if (reveal_mode == "0"){
+        meaning_text.style.display = "none";
+        reveal_mode = 1;
+    }
+    else if (reveal_mode == "1"){
+        meaning_text.style.display = "block";
+        reveal_mode = 0;
+    }
+ 
+    
+    
+   
+   
 }
